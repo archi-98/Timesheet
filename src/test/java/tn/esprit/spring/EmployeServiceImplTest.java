1dcf807bc73498dd6816b0a79de1c2716085e980
takwa-haraghi <57456845+takwa-haraghi@users.noreply.github.com>
2021-11-03 17:39:14 +0100

    test

--- src/test/java/tn/esprit/spring/EmployeServiceImplTest.java
+++ src/test/java/tn/esprit/spring/EmployeServiceImplTest.java
@@ -1,5 +1,7 @@
 package tn.esprit.spring;
 
+import java.util.Date;
+
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
@@ -8,7 +10,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 
 import tn.esprit.spring.entities.Entreprise;
-
+import tn.esprit.spring.entities.Mission;
 import tn.esprit.spring.entities.Employe;
 import tn.esprit.spring.entities.Role;
 
@@ -20,21 +22,71 @@ public class EmployeServiceImplTest {
 IEmployeService es;
 
 
-@Test
-@Order(10)
-public int testgetNombreEmployeJPQL(){
-	
-  return es.getNombreEmployeJPQL();
-	
-}
 
  
 @Test
 @Order(1)
 public void testAjouterEmploye(){
-	Employe e=new Employe("othmeni", "eya", "eya@esprit.tn", true, Role.INGENIEUR);
+	Employe e=new Employe("haraghi", "takwa", "takwa@esprit.tn", true, Role.INGENIEUR);
 	int id=es.ajouterEmploye(e);
 	Assertions.assertEquals(id,e.getId());
 }
+@Test
+@Order(2)
+public void testGetNombreEmployeJPQL(){
+	
+  es.getNombreEmployeJPQL();
+	
+}
+@Test
+@Order(3)
+public void TestGetAllEmployeNamesJPQL()
+{
+	es.getAllEmployeNamesJPQL();
+}
 
+
+@Test
+@Order(4)
+public void TestGetAllEmployeByEntreprise()
+{
+	Entreprise entreprise=new Entreprise("Esprit","RS");
+ es.getAllEmployeByEntreprise(entreprise);
+}
+@Test 
+@Order(5)
+public void TestMettreAjourEmailByEmployeIdJPQL(String email, int employeId)
+{
+es.mettreAjourEmailByEmployeIdJPQL(email, employeId);
+}
+@Test
+@Order(6)
+public void TestDeleteAllContratJPQL()
+{
+	es.deleteAllContratJPQL();
+	}
+@Test
+@Order(7)
+public void TestgetSalaireByEmployeIdJPQL(int employeId)
+{
+	es.getSalaireByEmployeIdJPQL(employeId);
+	}
+@Test
+@Order(8)
+public void TestGetSalaireMoyenByDepartementId(int departementId)
+{
+es.getSalaireMoyenByDepartementId(departementId);
+}
+@Test
+@Order(9)
+public void TestGetTimesheetsByMissionAndDate(Employe employe, Mission mission,Date dateDebut,Date dateFin)
+{
+	es.getTimesheetsByMissionAndDate(employe, mission, dateDebut, dateFin);
+}
+@Test
+@Order(10)
+public void TestAffecterEmployeADepartement(int employeId, int depId)
+{
+	es.affecterEmployeADepartement(employeId, depId);
+}
 }
