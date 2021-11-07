30d90adb9cea4d6bfb325a54425231e05f34a053
NourElhoudaNasri <78796864+NourElhoudaNasri@users.noreply.github.com>
2021-11-07 18:39:14 +0100

    add assertions

--- src/test/java/tn/esprit/spring/EmployeServiceImplTest.java
+++ src/test/java/tn/esprit/spring/EmployeServiceImplTest.java
@@ -14,18 +14,21 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 import tn.esprit.spring.entities.Contrat;
 
-import tn.esprit.spring.entities.Entreprise;
-import tn.esprit.spring.entities.Mission;
+
 import tn.esprit.spring.entities.Employe;
 import tn.esprit.spring.entities.Role;
-
+import tn.esprit.spring.repository.ContratRepository;
+import tn.esprit.spring.repository.EmployeRepository;
 import tn.esprit.spring.services.IEmployeService;
 
 @SpringBootTest
 public class EmployeServiceImplTest {
 @Autowired
 IEmployeService es;
-
+@Autowired
+EmployeRepository er;
+@Autowired
+ContratRepository cr;
 
 @Test
 @Order(1)
@@ -38,7 +41,8 @@ public void testAjouterEmploye(){
 @Test
 @Order(2)
 public void testMettreAjourEmailByEmployeId(){
-es.mettreAjourEmailByEmployeId("takwa.hraghi@esprit.tn", 40);
+es.mettreAjourEmailByEmployeId("takwa.hraghi@esprit.tn", 3);
+Assertions.assertEquals("takwa.hraghi@esprit.tn",er.findById(3).get().getEmail());
 }
 @Test
 @Order(3)
@@ -61,24 +65,29 @@ public void testAjouterContrat()throws ParseException {
 @Test
 @Order(5)
 public void testAffecterContratAEmploye(){
-	es.affecterContratAEmploye(11, 1);
+	es.affecterContratAEmploye(23,1);
 }
 
 @Test
 @Order(6)
 public void testGetEmployePrenomById(){
 	es.getEmployePrenomById(1);
+	Assertions.assertEquals("takwa",es.getEmployePrenomById(1));
 }
 @Test
 @Order(7)
 public void testDeleteEmployeById(){
 	es.deleteEmployeById(12);
+	Assertions.assertNull(er.findById(12).orElse(null));
+	
 }
 
 @Test
 @Order(8)
 public void testDeleteContratById(){
 	es.deleteContratById(11);
+	Assertions.assertNull(cr.findById(11).orElse(null));
+
 }
 @Test
 @Order(9)
